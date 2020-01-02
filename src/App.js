f233ce04ca15a7f790af3bff6bbf43eaad58d508
Lucas Sônego <lucassonego@ufpr.br>
2020-01-02 15:58:13 -0200

    importar imagens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import "./App.css";
+import profile from "./assets/gnome.jpg";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <img src={profile} width="200" />
+    </>
+  );
 }
 
 export default App;
